dad62b5099896547aa0df9ccc65441a94f834c0f
Pete Corey <petecorey@gmail.com>
2023-03-15 10:54:28 -0600

    Move data files into priv so they're bundled with release

--- lib/ffaker.ex
+++ lib/ffaker.ex
@@ -1,5 +1,5 @@
 defmodule Ffaker do
-  @moduledoc"""
+  @moduledoc """
   Common modules
   """
 
@@ -9,14 +9,15 @@ defmodule Ffaker do
         path =
           __MODULE__
           |> to_string
-          |> Macro.underscore
+          |> Macro.underscore()
           |> String.replace(~r(elixir.ffaker.|_test), "")
+
         Ffaker.list_file(file_name, path)
       end
     end
   end
 
-  @doc"""
+  @doc """
   make list from file
 
   ## Parameters
@@ -29,19 +30,22 @@ defmodule Ffaker do
       iex> Ffaker.list_file("numbers", "test")
       ["1", "2", "3"]
   """
-  @spec list_file(String.t, String.t) :: Enum.t
+  @spec list_file(String.t(), String.t()) :: Enum.t()
   def list_file(file_name, dir_path) do
-    file_path = "#{__DIR__}/../data/#{dir_path}/#{file_name}"
+    file_path = "#{:code.priv_dir(:ffaker)}/data/#{dir_path}/#{file_name}"
+
     case File.read(file_path) do
       {:ok, res} ->
         res
-        |> String.trim
+        |> String.trim()
         |> String.split("\n")
-      {:error, _} -> raise RuntimeError, message: "#{file_path} not found"
+
+      {:error, _} ->
+        raise RuntimeError, message: "#{file_path} not found"
     end
   end
 
-  @doc"""
+  @doc """
   replace # with number
 
   ## Parameters
@@ -53,16 +57,16 @@ defmodule Ffaker do
       iex> Ffaker.number("##-###")
       "12-345"
   """
-  @spec numerify(String.t) :: String.t
+  @spec numerify(String.t()) :: String.t()
   def numerify(string) do
     string
-    |> String.graphemes
+    |> String.graphemes()
     |> Enum.map(fn x ->
-         case x do
-           "#" -> 0..9 |> Enum.random |> to_string
-           x -> x
-         end
-       end)
-    |> Enum.join
+      case x do
+        "#" -> 0..9 |> Enum.random() |> to_string
+        x -> x
+      end
+    end)
+    |> Enum.join()
   end
 end
